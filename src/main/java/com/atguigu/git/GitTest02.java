7cf71c347678efb38675f0557cefe980f61a474f
gujie <1982727728@gujie>
2024-08-04 18:21:58 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest02.java
+++ src/main/java/com/atguigu/git/GitTest02.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest02 {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
